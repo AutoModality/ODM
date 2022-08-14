a434db835dc3d6fb4f7d731cf7114f0bb4368458
usplm <admin@usplm.net>
2022-08-14 09:41:42 -0400

    Exchange template and input images

--- opendm/multispectral.py
+++ opendm/multispectral.py
@@ -562,7 +562,7 @@ def find_ecc_homography(image_gray, align_image_gray, number_of_iterations=1000,
 
         try:            
             log.ODM_INFO("Computing ECC pyramid level %s using Gaussian filter size %s" % (level, gaussian_filter_size))
-            _, warp_matrix = cv2.findTransformECC(aig, ig, warp_matrix, cv2.MOTION_HOMOGRAPHY, criteria, inputMask=None, gaussFiltSize=gaussian_filter_size)
+            _, warp_matrix = cv2.findTransformECC(ig, aig, warp_matrix, cv2.MOTION_HOMOGRAPHY, criteria, inputMask=None, gaussFiltSize=gaussian_filter_size)
         except Exception as e:
             if level != pyramid_levels:
                 log.ODM_INFO("Could not compute ECC warp_matrix at pyramid level %s, resetting matrix" % level)
