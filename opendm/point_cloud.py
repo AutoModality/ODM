c58f6092ac2c6a339f2f5200f2671aac3b4a053c
Yunpeng Li <admin@usplm.net>
2024-12-17 16:29:30 -0500

    Disable pc classification using OpenPointClass

--- opendm/point_cloud.py
+++ opendm/point_cloud.py
@@ -37,7 +37,7 @@ def ply_info(input_ply):
             i += 1
             if i > 100:
                 raise IOError("Cannot find end_header field. Invalid PLY?")
-            
+
 
     return {
         'has_normals': has_normals,
@@ -62,7 +62,7 @@ def split(input_point_cloud, outdir, filename_template, capacity, dims=None):
         sys.exit(1)
 
     cmd = 'pdal split -i "%s" -o "%s" --capacity %s ' % (input_point_cloud, os.path.join(outdir, filename_template), capacity)
-    
+
     if filename_template.endswith(".ply"):
         cmd += ("--writers.ply.sized_types=false "
                 "--writers.ply.storage_mode=\"little endian\" ")
@@ -97,7 +97,7 @@ def filter(input_point_cloud, output_point_cloud, output_stats, standard_deviati
         args.append('--meank %s' % meank)
         args.append('--std %s' % standard_deviation)
         args.append('--stats "%s"' % output_stats)
-    
+
     if boundary is not None:
         log.ODM_INFO("Boundary {}".format(boundary))
         fd, boundary_json_file = tempfile.mkstemp(suffix='.boundary.json')
@@ -119,7 +119,7 @@ def get_spacing(stats_file, resolution_fallback=5.0):
 
     if not os.path.isfile(stats_file):
         return fallback()
-    
+
     with open(stats_file, 'r') as f:
         j = json.loads(f.read())
         if "spacing" in j:
@@ -141,7 +141,7 @@ def export_summary_json(pointcloud_path, summary_file_path):
 def get_extent(input_point_cloud):
     fd, json_file = tempfile.mkstemp(suffix='.json')
     os.close(fd)
-    
+
     # Get point cloud extent
     fallback = False
 
@@ -160,7 +160,7 @@ def get_extent(input_point_cloud):
     bounds = {}
     with open(json_file, 'r') as f:
         result = json.loads(f.read())
-        
+
         if not fallback:
             summary = result.get('summary')
             if summary is None: raise Exception("Cannot compute summary for %s (summary key missing)" % input_point_cloud)
@@ -175,7 +175,7 @@ def get_extent(input_point_cloud):
             bounds = native.get('bbox')
 
         if bounds is None: raise Exception("Cannot compute bounds for %s (bounds key missing)" % input_point_cloud)
-        
+
         if bounds.get('maxx', None) is None or \
             bounds.get('minx', None) is None or \
             bounds.get('maxy', None) is None or \
@@ -183,7 +183,7 @@ def get_extent(input_point_cloud):
             bounds.get('maxz', None) is None or \
             bounds.get('minz', None) is None:
             raise Exception("Cannot compute bounds for %s (invalid keys) %s" % (input_point_cloud, str(bounds)))
-            
+
     os.remove(json_file)
     return bounds
 
@@ -204,7 +204,7 @@ def merge(input_point_cloud_files, output_file, rerun=False):
     }
 
     system.run('lasmerge -i {all_inputs} -o "{output}"'.format(**kwargs))
-   
+
 
 def fast_merge_ply(input_point_cloud_files, output_file):
     # Assumes that all input files share the same header/content format
@@ -214,11 +214,11 @@ def fast_merge_ply(input_point_cloud_files, output_file):
     if num_files == 0:
         log.ODM_WARNING("No input point cloud files to process")
         return
-    
+
     if io.file_exists(output_file):
         log.ODM_WARNING("Removing previous point cloud: %s" % output_file)
         os.remove(output_file)
-    
+
     vertex_count = sum([ply_info(pcf)['vertex_count'] for pcf in input_point_cloud_files])
     master_file = input_point_cloud_files[0]
     with open(output_file, "wb") as out:
@@ -230,7 +230,7 @@ def fast_merge_ply(input_point_cloud_files, output_file):
             i = 0
             while line.strip().lower() != "end_header":
                 line = fhead.readline()
-                
+
                 # Intercept element vertex field
                 if line.lower().startswith("element vertex "):
                     out.write(("element vertex %s\n" % vertex_count).encode('utf8'))
@@ -240,7 +240,7 @@ def fast_merge_ply(input_point_cloud_files, output_file):
                 i += 1
                 if i > 100:
                     raise IOError("Cannot find end_header field. Invalid PLY?")
-            
+
         for ipc in input_point_cloud_files:
             i = 0
             with open(ipc, "rb") as fin:
@@ -252,10 +252,10 @@ def fast_merge_ply(input_point_cloud_files, output_file):
                     i += 1
                     if i > 100:
                         raise IOError("Cannot find end_header field. Invalid PLY?")
-                
+
                 # Write fields
                 out.write(fin.read())
-    
+
     return output_file
 
 
@@ -284,14 +284,14 @@ def post_point_cloud_steps(args, tree, rerun=False):
         if not io.file_exists(pc_classify_marker) or rerun:
             log.ODM_INFO("Classifying {} using Simple Morphological Filter (1/2)".format(tree.odm_georeferencing_model_laz))
             commands.classify(tree.odm_georeferencing_model_laz,
-                                args.smrf_scalar, 
-                                args.smrf_slope, 
-                                args.smrf_threshold, 
+                                args.smrf_scalar,
+                                args.smrf_slope,
+                                args.smrf_threshold,
                                 args.smrf_window
                             )
 
-            log.ODM_INFO("Classifying {} using OpenPointClass (2/2)".format(tree.odm_georeferencing_model_laz))
-            classify(tree.odm_georeferencing_model_laz, args.max_concurrency)
+            log.ODM_INFO("Classifying {} using OpenPointClass (2/2) ... Skipped".format(tree.odm_georeferencing_model_laz))
+            # classify(tree.odm_georeferencing_model_laz, args.max_concurrency)
 
             with open(pc_classify_marker, 'w') as f:
                 f.write('Classify: smrf\n')
@@ -299,14 +299,14 @@ def post_point_cloud_steps(args, tree, rerun=False):
                 f.write('Slope: {}\n'.format(args.smrf_slope))
                 f.write('Threshold: {}\n'.format(args.smrf_threshold))
                 f.write('Window: {}\n'.format(args.smrf_window))
-    
+
     if args.pc_rectify:
         commands.rectify(tree.odm_georeferencing_model_laz)
 
     # XYZ point cloud output
     if args.pc_csv:
         log.ODM_INFO("Creating CSV file (XYZ format)")
-        
+
         if not io.file_exists(tree.odm_georeferencing_xyz_file) or rerun:
             system.run("pdal translate -i \"{}\" "
                 "-o \"{}\" "
@@ -321,7 +321,7 @@ def post_point_cloud_steps(args, tree, rerun=False):
     # LAS point cloud output
     if args.pc_las:
         log.ODM_INFO("Creating LAS file")
-        
+
         if not io.file_exists(tree.odm_georeferencing_model_las) or rerun:
             system.run("pdal translate -i \"{}\" "
                 "-o \"{}\" ".format(
