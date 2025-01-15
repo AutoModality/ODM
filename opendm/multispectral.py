1c0d41df88e4feddc689e13f0ebdca79313a7bdd
Yunpeng Li <admin@usplm.net>
2025-01-15 14:53:53 -0500

    Add special handling of use_local_warp_matrix option for MicaSense sensors

--- opendm/multispectral.py
+++ opendm/multispectral.py
@@ -348,7 +348,14 @@ def compute_alignment_matrices(multi_camera, primary_band_name, images_path, s2p
     for band in multi_camera:
         if band['name'] != primary_band_name:
             matrices_samples = []
-            use_local_warp_matrix = use_local_homography # and band['name'] == 'LWIR'
+            use_local_warp_matrix = use_local_homography
+
+            # For MicaSense sensors, only apply use_local_warp_matrix to the LWIR band
+            first_photo = band['photos'][0]
+            if first_photo is not None:
+                if use_local_warp_matrix and first_photo.camera_make == 'MicaSense' and first_photo.band_name != 'LWIR':
+                    use_local_warp_matrix = False
+
             max_samples = max_samples if not use_local_warp_matrix and max_samples > 0 and max_samples < len(band['photos']) else len(band['photos'])
 
             def parallel_compute_homography(photo):
